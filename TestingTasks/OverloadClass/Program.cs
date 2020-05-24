d4cab1a87305b9b7ca4c1de3ec08ae764b522846
suga23 <Taurus1024@yandex.ru>
2020-05-24 17:48:52 +0300

    Машина ч.1

--- TestingTasks/OverloadClass/Program.cs
+++ TestingTasks/OverloadClass/Program.cs
@@ -1,48 +1,26 @@
-﻿using Microsoft.CSharp.RuntimeBinder;
-using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Dynamic;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-// Создать класс Money для работы с денежными суммами в котором для рублей и копеек предусмотрены независимые целочисленные данные. Реализовать метод вывода суммы на экран. 
-// На основе класса Money создать класс Good для работы с товаром. Предусмотреть метод, осуществляющий уменьшение цены на заданное число процентов. 
-namespace TestingTasks
-{
-    class Money
-    {
-        decimal sum;
-        public Money(decimal rub, decimal cent)
-        {
-            sum = rub + (cent / 100);
-            Console.WriteLine($"Сумма без скидки равна {sum}");
-        }
-        public Money(decimal rub, decimal cent, decimal percent)
-        {
-            if (percent >= 100)
-            {
-                Console.WriteLine("Ошибка.");
-            }
-            else
-            {
-                cent += rub * 100;
-                rub = cent - (cent * percent / 100);
-                sum = Math.Round(rub / 100, 2);
-                Console.WriteLine($"Сумма за вычетом скидки равна {sum}");
-            }
-        }
-    }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+// Разработать программу, в которой предусмотреть наследование минимум трех классов, например, средство передвижения, колесное средство передвижения, автомобиль, легковой автомобиль.
+// В программе должен использоваться принцип полиморфизма. Предусмотреть накопление объектов классов в структуре типа массив, список или очередь, но не стек.
+namespace OverloadClass
+{    
     class Program
     {
-        static void Main()
+        
+        static void Main(string[] args)
         {
-            Money[] ob = new Money[5];
-            ob[0] = new Money(100, 550);
-            ob[1] = new Money(500, 68);
-            ob[2] = new Money(500, 720, 5);
-            ob[3] = new Money(250, 0, 50);
-            ob[4] = new Money(1500, 250, 100);
+            LightCar ob = new LightCar("BMW", 250, "Красный", "Летняя резина");
+            Console.WriteLine(ob.Show());
+            //List<Cargo> ob = new List<Cargo>();
+            //ob.Add(new Cargo() { machineMark = "Зил", speed = 120, color = "Красный.", wheelsType = "Летняя резина", wheels = 8, bodyType = "Открытый" });
             Console.ReadLine();
         }
     }
 }
+//                      string machineMark, double speed, string color, string wheelsType, int wheels,  string bodyType) : base(machineMark, speed, wheelsType, color
+//, speed = 120, color = "Красный", wheelsType = "Летняя резина", wheels = 8, 
