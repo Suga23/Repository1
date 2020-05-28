37b44d6442c19902ea33f0304aa7f053270cfea9
suga23 <Taurus1024@yandex.ru>
2020-05-28 14:49:17 +0300

    New task

--- TestingTasks/OverloadClass/Program.cs
+++ TestingTasks/OverloadClass/Program.cs
@@ -2,25 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
-// Разработать программу, в которой предусмотреть наследование минимум трех классов, например, средство передвижения, колесное средство передвижения, автомобиль, легковой автомобиль.
-// В программе должен использоваться принцип полиморфизма. Предусмотреть накопление объектов классов в структуре типа массив, список или очередь, но не стек.
+/*Создайте структуру с именем student, содержащую поля: фамилия и инициалы, номер группы, 
+успеваемость (массив из пяти элементов). Создать массив из десяти элементов такого типа,
+упорядочить записи по возрастанию среднего балла. Добавить возможность вывода фамилий и 
+номеров групп студентов, имеющих оценки, равные только 4 или 5.*/
 namespace OverloadClass
-{    
-    class Program
+{
+    struct Student
     {
-        
-        static void Main(string[] args)
+        public string lastName;
+        public string initials;
+        public int groupNumber;
+        public int[] academicPerformance;
+    }
+    class Middle
+    {
+        Student st;
+        int Mark;
+        int Mark1;
+        public Middle(string lN, string ini, int gN, params int[] aP)
         {
-            LightCar ob = new LightCar("BMW", 250, "Красный", "Летняя резина");
-            Console.WriteLine(ob.Show());
-            //List<Cargo> ob = new List<Cargo>();
-            //ob.Add(new Cargo() { machineMark = "Зил", speed = 120, color = "Красный.", wheelsType = "Летняя резина", wheels = 8, bodyType = "Открытый" });
-            Console.ReadLine();
+            st.lastName = lN;
+            st.initials = ini;
+            st.groupNumber = gN;
+            st.academicPerformance = aP;
         }
-    }////алвтмлв
+        public void Show()
+        {
+            for (int i = 0; i < st.academicPerformance.Length; i++)
+            {
+                Mark1 = st.academicPerformance[i];
+            }
+
+            for (int i = 0; i < st.academicPerformance.Length; i++)
+            {
+                Mark += st.academicPerformance[i];
+            }
+
+            Console.WriteLine($"Фамилия: {st.lastName}\nИнициалы: {st.initials}\nНомер группы: {st.groupNumber}\nУспеваемость: {Mark1}\nСредний бал: {Mark / st.academicPerformance.Length}");
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Middle[] students = new Middle[10];
+                Console.WriteLine("Введите данные первого студента: ");
+                int[] arr = new int[5] { 5, 3, 5, 3, 4 };
+                students[0] = new Middle("Belugin", "P.A.", 1, arr);
+                students[0].Show();
+                Console.ReadLine();
+            }
+        }
+    }
 }
-//                      string machineMark, double speed, string color, string wheelsType, int wheels,  string bodyType) : base(machineMark, speed, wheelsType, color
-//, speed = 120, color = "Красный", wheelsType = "Летняя резина", wheels = 8, 
--- TestingTasks/OverloadClass/WheelCar.cs
+++ TestingTasks/OverloadClass/WheelCar.cs
@@ -1,69 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OverloadClass
 {
-    abstract class Vehicle
-    {
-        public string machineMark, color, wheelsType;
-        public double speed;
-        public Vehicle(string machineMark, double speed, string wheelsType, string color)
-        {
-            this.machineMark = machineMark;
-            this.speed = speed;
-            this.wheelsType = wheelsType;
-            this.color = color;
-        }
-        public abstract string Show();
-    }
-    class WheelCar : Vehicle
-    {
-
-        public WheelCar(string machineMark, double speed, string wheelsType, string color) : base(machineMark, speed, wheelsType, color)
-        {
-
-        }
-        public override string Show()
-        {
-            return "Колёсный автомобиль представляет из себя бла-бла-бла.";
-        }
-
-
-    }
-
-    class Cargo : WheelCar
-    {
-        string bodyType; // machineMark, color, wheelsType, 
-        //double speed;
-        int wheels;
-        public Cargo(string machineMark, double speed, string color, string wheelsType, int wheels,  string bodyType) : base(machineMark, speed, wheelsType, color)
-        {
-            //this.machineMark = machineMark;
-            //this.color = color;
-            //this.wheelsType = wheelsType;
-            //this.speed = speed;
-            this.wheels = wheels;
-            this.bodyType = bodyType;
-        }
-        public override string Show()
-        {
-            return $"Марка автомобиля:  {machineMark}\nМаксимальная скорость автомобиля:    {speed}\nЦвет автомобиля:  {color} км/ч\nТип колёс автомобиля:   {wheelsType}\nКоличество колёс: {wheels}\nТип кузова:   {bodyType}";
-        }
-    }
-
-    class LightCar : WheelCar
-    {
-        public LightCar(string machineMark, double speed, string color, string wheelsType) : base(machineMark, speed, wheelsType, color)
-        {
-
-        }
-
-        public override string Show()
-        {
-            return $"Марка автомобиля:  {machineMark}\nМаксимальная скорость автомобиля:    {speed}\nЦвет автомобиля:  {color} км/ч\nТип колёс автомобиля:   {wheelsType}";
-        }
-    }
+    
 }
