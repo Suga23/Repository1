e1d1f86eadf287afab9ff6d5a2848c140ea40546
suga23 <Taurus1024@yandex.ru>
2020-06-17 16:17:44 +0300

    12345

--- TestingTasks/DynamicArray/Program.cs
+++ TestingTasks/DynamicArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,59 @@ using System.Xml.Schema;
 * элементов, и деструктор. Добавить методы, позволяющие заполнять массив 
 * случайными числами, переставлять в данном массиве элементы в случайном порядке,
 * находить количество различных элементов в массиве, выводить массив на экран.*/
-//class MainArray1
-//{
-//    int[] arr
-//    public MainArray1(int size)
-//    {
-//        arr = new int[size];
-//         = size;
-//    }
-//namespace DynamicArray
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
+class MainArray1
+{
+    int[] arr;
+    int Length;
+    public MainArray1(int size)
+    {
+        arr = new int[size];
+        Length = size;
+    }
+    public int this[int index]
+    {
+        get
+        {
+            if(ArrayOverFlow(index))
+            {
+                return arr[index];
+            } else
+            {
+                throw new Exception("При генерации произошла ошибка.");
+            }
+        }
+        set
+        {
+            if(ArrayOverFlow(index))
+            {
+                arr[index] = value;
+            } else
+            {
+                while()
+                {
+                    arr[index++] = value;
+                }
+            }
+        }
+    }
+    private bool ArrayOverFlow(int index)
+    {
+        if(index >= 0 && index < Length)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+/* Необходимо сгенерировать массив с фиксированными значениями, разработать внутренний метод в котором, 
+ * если происходит переполнение массива, то увеличивается длина этого массива и в открывшуюся ячейку добвляется наше значение */
+namespace DynamicArray
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
 
-//        }
-//    }
-//}
+        //}
+    }
+}
